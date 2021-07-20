c462f1680b843ba4274ce6443d1b8a5d64b68713
HeumHeum2 <jwh6295@gmail.com>
2021-07-20 22:29:46 +0900

    feat: navigator, 서비스, 버튼 컴포넌트 설정

--- src/screens/__tests__/WeatherScreen.test.tsx
+++ src/screens/__tests__/WeatherScreen.test.tsx
@@ -1,6 +1,6 @@
 import {render} from '@testing-library/react-native';
 import React from 'react';
-import WeatherScreen from '../../screens/WeatherScreen';
+import WeatherScreen from '../WeatherScreen';
 
 describe('WeatherScreen', () => {
   test('Should render correctly', () => {
