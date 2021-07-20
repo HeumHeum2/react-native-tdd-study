470ddc98ee4beccd74e4ad64b1dfea71f2d8e909
HeumHeum2 <jwh6295@gmail.com>
2021-07-21 00:31:08 +0900

    feat: 위치 값 받아오는 폼 작성

--- src/components/WeatherCoordinates.tsx
+++ src/components/WeatherCoordinates.tsx
@@ -1,8 +1,97 @@
 import React from 'react';
-import {View} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {Controller, useForm} from 'react-hook-form';
+import {View, StyleSheet, TextInput} from 'react-native';
+import * as Yup from 'yup';
+import {yupResolver} from '@hookform/resolvers/yup';
+import {Colors} from '../constants';
+import Button from './Button';
+
+type FormValues = {
+  latitude: string;
+  longitude: string;
+};
 
 function WeatherCoordinates() {
-  return <View testID="weather-coordinates" />;
+  const navigation = useNavigation();
+
+  const form = useForm<FormValues>({
+    resolver: yupResolver(validationSchema),
+    defaultValues,
+    mode: 'onChange',
+  });
+
+  const handleSubmit = form.handleSubmit(values => {
+    navigation.navigate('Weather', values);
+  });
+  return (
+    <View testID="weather-coordinates">
+      <View style={styles.inputs}>
+        <Controller
+          control={form.control}
+          render={({field}) => {
+            const {onChange, ...props} = field;
+            return (
+              <TextInput
+                {...props}
+                testID="weather-coordinates-latitude"
+                onChangeText={onChange}
+                style={styles.input}
+                placeholder="Lat"
+                placeholderTextColor={Colors.GRAY}
+              />
+            );
+          }}
+          name="latitude"
+        />
+        <Controller
+          control={form.control}
+          render={({field}) => {
+            const {onChange, ...props} = field;
+            return (
+              <TextInput
+                {...props}
+                testID="weather-coordinates-longitude"
+                onChangeText={onChange}
+                style={styles.input}
+                placeholder="Long"
+                placeholderTextColor={Colors.GRAY}
+              />
+            );
+          }}
+          name="longitude"
+        />
+      </View>
+      <Button onPress={handleSubmit} label="find" />
+    </View>
+  );
 }
+
+const defaultValues: FormValues = {
+  latitude: '',
+  longitude: '',
+};
+
+const validationSchema = Yup.object().shape({
+  latitude: Yup.number(),
+  longitude: Yup.number(),
+});
+
+const styles = StyleSheet.create({
+  inputs: {
+    flexDirection: 'column',
+    marginBottom: 15,
+  },
+  input: {
+    backgroundColor: Colors.TRANSPARENT,
+    margin: 5,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: Colors.GRAY,
+    paddingHorizontal: 15,
+    paddingVertical: 8,
+    color: Colors.WHITE,
+  },
+});
 
 export default WeatherCoordinates;
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -13,3 +13,6 @@ jest.mock('react-native-reanimated', () => {
 });
 
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+
+// @ts-ignore
+global.window = {};
